4201562f05b6b279cd620a5f9cfc35fc35447cd7
eyyleen <eileen.putri28@gmail.com>
2025-06-02 20:23:42 +0700

    Delete FinalProjectPWD directory

--- FinalProjectPWD/ceklog.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-if (!isset($_SESSION['login']) || $_SESSION['login'] !== true) {
-$_SESSION['error'] = "Silakan login terlebih dahulu.";
-header("Location: login.php");
-exit();
-}
-?>
